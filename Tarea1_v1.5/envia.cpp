5fb9a02bbd7aec9179f5871efd49d26c51b42495
xornet93 <44046901+xornet93@users.noreply.github.com>
2018-10-21 22:17:54 -0300

    Update envia.cpp
    Menu

--- Tarea1_v1.5/envia.cpp
+++ Tarea1_v1.5/envia.cpp
@@ -1,20 +1,44 @@
+
 #include "funciones.h"
 #define U_TIME 50000
 #define PUERTO 17
+#define CANTIDAD 10000
+
+
+int opcion;
+char aux;
+int c=0, cont=0;
+
+void menu(int opcion);
+
 int main(){
-	//int c = 0;
-	openPort( PUERTO , U_TIME ) ;
-	Paquete p;
-	p.cmd=3;
-	while(true){//(c<=10000){
-		printf("Cual es su mensaje? : ");
-		fgets((char*)p.data,N,stdin);
-		int size = strlen((char*)p.data);
-		size--;
-		p.Long=size;
-		envia(PUERTO,p);
-		//c++;
-	}
-	//closePort(puerto);
-	return 0;
+        printf("\n\t Menu\n\n1. Enviar Mensaje\n2. Cerrar receptor y programa\n\nElija una opcion: ");
+        scanf(" %d",&opcion);
+        Paquete p;
+        int size;
+        switch(opcion){
+                case 1:
+                        scanf("%c",&aux);
+                        p.cmd=1;
+                        printf("\tCual es su mensaje? : ");
+                        fgets((char*)p.data,N,stdin);
+                        printf("\nEnviando Mensaje.. \n");
+                        size= strlen((char*)p.data);
+                        openPort( PUERTO , U_TIME );
+                        while(cont<=CANTIDAD){
+                                size--;
+                                p.Long=size;
+                                envia(PUERTO,p);
+                                cont++;
+                        }
+                        break;
+                case 2:
+                        p.cmd=7;
+                        return 0;
+                        break;
+                }
+        return 0;
 }
+
+
+
